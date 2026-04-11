9b3ca61fa8c9b440d4e5e8cf1b17054b01fc9497
kazi-ziaur-rahman-majba <majba9399@gmail.com>
2026-04-11 16:55:57 +0600

    added vercel.json file deploying purpose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { ValidationPipe, ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
+
+let cachedApp: INestApplication;
 
 async function bootstrap() {
+  if (cachedApp) return cachedApp;
+
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
@@ -21,6 +25,24 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  await app.listen(process.env.PORT ?? 5000);
+  await app.init();
+  cachedApp = app;
+  return app;
 }
-bootstrap();
+
+// Export for Vercel serverless function
+export default async (req: any, res: any) => {
+  const app = await bootstrap();
+  const instance = app.getHttpAdapter().getInstance();
+  instance(req, res);
+};
+
+// Start locally if not running on Vercel
+if (!process.env.VERCEL) {
+  bootstrap().then((app) => {
+    const port = process.env.PORT ?? 5000;
+    app.listen(port).then(() => {
+      console.log(`Application is running on: http://localhost:${port}`);
+    });
+  });
+}
